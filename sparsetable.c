bd4414f610781229b90ccfa40c60870fda3a3e4b
Thayna Santos <tecs@cin.ufpe.br>
2023-11-25 20:52:57 -0300

    Sparse Table algorithm finalizado

--- sparsetable.c
+++ sparsetable.c
@@ -1,112 +1,138 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-using namespace std;
-
+#define ui32 uint32_t
 #define A 1664525
 #define C 1013904223
 
-string FUN;
-uint32_t Seed;
-int COL, L, T_Op, Q, U;
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
-int current;
+ui32 current;
+int *LOG;
 
-int NEXT_RAND(int current);
+ui32 Seed;
+char FUN[4];
+int COL, L, T_Op, Q, U, M;
+
+ui32 NEXT_RAND();
+void PRECOMPUTE_LOGS();
 int QRY(int L, int R, int *sparse_table[]);
 void UPD(int index, int *sparse_table[]);
-int LOG2(int number);
 
 int main()
 {
     int case_number = 0;
 
-    while (1)
+    while (scanf("%u", &Seed) == 1)
     {
-        cin >> Seed >> COL >> FUN >> T_Op >> Q >> U;
+        scanf("%d", &COL);
+        scanf("%s", FUN);
+        scanf("%d", &T_Op);
+        scanf("%d", &Q);
+        scanf("%d", &U);
 
-        L = LOG2(COL) + 1; // + 1 to include the line 0
+        PRECOMPUTE_LOGS();
+
+        L = LOG[COL - 1] + 1; // + 1 to include the line 0
+        M = 4 * COL;
 
         int *sparse_table[L];
         for (int i = 0; i < L; i++)
-            sparse_table[i] = new int[COL];
+            sparse_table[i] = (int *)malloc(COL * sizeof(int));
 
         current = Seed;
 
         // PRE-PROCESSAMENTO 1
         for (int i = 0; i < COL; i++)
             if (i == 0)
-                sparse_table[0][i] = current;
+                sparse_table[0][i] = (Seed % M);
             else
-                sparse_table[0][i] = NEXT_RAND(current);
+                sparse_table[0][i] = (NEXT_RAND() % M);
 
         // PRE-PROCESSAMENTO 2
         for (int i = 1; i < L; i++)
             for (int j = 0; j + (1 << i) - 1 < COL; j++)
-                if (FUN == "MAX")
+                if (FUN[1] == 'A')
                     sparse_table[i][j] = max(sparse_table[i - 1][j],
                                              sparse_table[i - 1][j + (1 << (i - 1))]);
-                else if (FUN == "MIN")
+                else if (FUN[1] == 'I')
                     sparse_table[i][j] = min(sparse_table[i - 1][j],
                                              sparse_table[i - 1][j + (1 << (i - 1))]);
+
                 else
                     sparse_table[i][j] = sparse_table[i - 1][j] +
                                          sparse_table[i - 1][j + (1 << (i - 1))];
 
         // SIMULADOR DE OPERACOES
-        cout << "caso " << case_number << endl;
+        printf("caso %d\n", case_number);
 
         for (int i = 0; i < T_Op; i++)
         {
-            int x = NEXT_RAND(current);
-            string OP = (x % (Q + U)) < Q ? "QRY" : "UPD";
-
+            ui32 x = NEXT_RAND();
             int result;
 
-            if (OP == "QRY")
+            if ((x % (Q + U)) < Q)
             {
-                int L = NEXT_RAND(current) % COL;
-                int R = L + 1 + (NEXT_RAND(current) % (COL - L));
+                int L = (NEXT_RAND() % COL);
+                int R = L + 1 + (NEXT_RAND() % (COL - L));
 
                 result = QRY(L, R, sparse_table);
             }
             else
             {
-                int index = NEXT_RAND(current) % COL;
+                int index = (NEXT_RAND() % COL);
                 UPD(index, sparse_table);
                 result = QRY(index, COL, sparse_table);
             }
 
-            cout << result << endl;
+            printf("%d\n", result);
         }
 
-        case_number++;
+        printf("\n");
+
         for (int i = 0; i < L; i++)
-            delete[] sparse_table[i];
-        }
+            free(sparse_table[i]);
+
+        free(LOG);
+
+        case_number++;
+    }
 
     return 0;
 }
 
-int NEXT_RAND(int current) { return ((A * current) + C) % (4 * COL); }
-
-int LOG2(int number)
+ui32 NEXT_RAND()
 {
-    int k = 0;
-    while ((1 << (k + 1)) <= number)
-        k++;
-    return k;
+    current = (A * current) + C;
+    return current;
+}
+
+void PRECOMPUTE_LOGS()
+{
+    LOG = (int *)malloc(COL * sizeof(int));
+
+    int k;
+    for (int i = 0; i < COL; i++)
+    {
+        int k = 0;
+        while ((1 << (k + 1)) <= i)
+            k++;
+        LOG[i] = k;
+    }
 }
 
 int QRY(int L, int R, int *sparse_table[])
 {
-    int max_fit = LOG2(R - L);
+    int max_fit = LOG[R - L - 1];
     int result;
 
-    if (FUN == "MAX")
-        result = max(sparse_table[max_fit][L], sparse_table[max_fit][R - max_fit - 1]);
+    if (FUN[1] == 'A')
+        result = max(sparse_table[max_fit][L], sparse_table[max_fit][R - (1 << max_fit)]);
 
-    else if (FUN == "MIN")
-        result = min(sparse_table[max_fit][L], sparse_table[max_fit][R - max_fit - 1]);
+    else if (FUN[1] == 'I')
+        result = min(sparse_table[max_fit][L], sparse_table[max_fit][R - (1 << max_fit)]);
 
     else
     {
@@ -116,7 +142,7 @@ int QRY(int L, int R, int *sparse_table[])
 
         while (left > 0)
         {
-            max_fit = LOG2(left);
+            max_fit = LOG[left - 1];
             result += sparse_table[max_fit][L];
 
             L += (1 << max_fit);
@@ -128,17 +154,17 @@ int QRY(int L, int R, int *sparse_table[])
 
 void UPD(int index, int *sparse_table[])
 {
-    int valor = NEXT_RAND(current) % (4 * COL);
+    int valor = (NEXT_RAND() % M);
 
     sparse_table[0][index] = valor;
 
     for (int line = 1; line < L; line++)
-        for (int col = max(0, index - (1 << line) + 1); col < index; col++)
+        for (int col = max(0, index - (1 << line) + 1); col <= index; col++)
         {
-            if (FUN == "MAX")
+            if (FUN[1] == 'A')
                 sparse_table[line][col] = max(sparse_table[line - 1][col],
                                               sparse_table[line - 1][(col + (1 << (line - 1)))]);
-            else if (FUN == "MIN")
+            else if (FUN[1] == 'I')
                 sparse_table[line][col] = min(sparse_table[line - 1][col],
                                               sparse_table[line - 1][(col + (1 << (line - 1)))]);
             else

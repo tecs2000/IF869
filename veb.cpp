759b8fdebc2642c543d618a74081e33402b2c05a
Thayna Santos <tecs@cin.ufpe.br>
2023-12-07 21:11:31 -0300

    Algoritmo de Van Emde Boas finalizado

--- veb.cpp
+++ veb.cpp
@@ -3,360 +3,285 @@
 using namespace std;
 
 #define ui unsigned int
-#define A 1664525
-#define C 1013904223
 
-ui Seed, Current;
-
-ui NEXT_RAND();
-
-class Van_Emde_Boas
+class RNG
 {
+private:
+    ui A;
+    ui C;
+    ui Last;
+    ui seed;
 
 public:
-    int universe_size;
-    int minimum;
-    int maximum;
-    int sqrt;
-    Van_Emde_Boas *summary;
-    vector<Van_Emde_Boas *> clusters;
-
-    int high(int x)
+    RNG(ui seed)
     {
-        return x / this->sqrt;
+        A = 1664525;
+        C = 1013904223;
+        this->Last = 0;
+        this->seed = seed;
     }
 
-    int low(int x)
+    ui next()
     {
-        return x % this->sqrt;
-    }
-
-    int generate_index(int x, int y)
-    {
-        return (x * this->sqrt) + y;
-    }
-
-    // Constructor
-    Van_Emde_Boas(int size)
-    {
-        universe_size = size;
-        minimum = universe_size;
-        maximum = -1;
-        sqrt = ceil(sqrt(universe_size));
-
-        // Base case
-        if (size > 2)
+        if (this->Last == 0)
         {
-            summary = new Van_Emde_Boas(sqrt);
-
-            clusters = vector<Van_Emde_Boas *>(sqrt, nullptr);
-
-            // cada cluster recebe uma VEB(universe_root)
-            for (int i = 0; i < sqrt; i++)
-                clusters[i] = new Van_Emde_Boas(sqrt);
+            this->Last = this->seed;
+            return this->seed;
+        }
+        else
+        {
+            this->Last = (this->A * this->Last) + this->C;
+            return this->Last;
         }
     }
 };
 
-int minimum(Van_Emde_Boas *veb)
+class VanEmdeBoas
 {
-    return (veb->minimum == veb->universe_size ? veb->universe_size : veb->minimum);
-}
+private:
+    int universeSize;
+    int minimum;
+    int maximum;
+    int universeRoot;
+    VanEmdeBoas *summary;
+    vector<VanEmdeBoas *> clusters;
 
-int maximum(Van_Emde_Boas *veb)
-{
-    return (veb->maximum == -1 ? -1 : veb->maximum);
-}
+public:
+    int high(int x) { return x / this->universeRoot; }
 
-int insert(Van_Emde_Boas *veb, int key, int nivel = 1)
-{
+    int low(int x) { return x % this->universeRoot; }
 
-    if (veb->universe_size == 2)
+    int generate_index(int x, int y) { return (x * this->universeRoot) + y; }
+    // Constructor
+    VanEmdeBoas(int size)
     {
-        veb->minimum = min(veb->minimum, key);
-        veb->maximum = max(veb->maximum, key);
+        this->universeSize = size;
+        this->universeRoot = sqrt(universeSize);
+        this->minimum = universeSize;
+        this->maximum = -1;
 
-        return nivel;
-    }
-
-    // se veb vazia, min = max = key
-    if (veb->minimum == veb->universe_size)
-    {
-        veb->minimum = key;
-        veb->maximum = key;
-
-        return nivel;
-    }
-    
-    if (key < veb->minimum)
-        swap(veb->minimum, key);
-
-    veb->maximum = max(veb->maximum, key);
-
-    int high = veb->high(key);
-    int low = veb->low(key);
-
-    // se cluster vazio, insere no cluster e no summary
-    if (veb->clusters[high]->minimum == veb->universe_size)
-        insert(veb->summary, high);
-
-    return insert(veb->clusters[high], low, ++nivel);
-}
-
-bool contains(Van_Emde_Boas *veb, int key)
-{
-
-    // A key está fora do universo
-    if (veb->universe_size < key)
-        return false;
-
-    if (veb->minimum == key || veb->maximum == key)
-        return true;
-    else
-    {
-        // se o tamanho do universo é 2 e a chave
-        // não é nem o max, nem o min, significa que ela
-        // não está presente na estrutura
-        if (veb->universe_size == 2)
-            return false;
-        else
+        // Base case
+        if (size > 2)
         {
-            return contains(
-                veb->clusters[veb->high(key)],
-                veb->low(key));
+            summary = new VanEmdeBoas(this->universeRoot);
+            clusters = vector<VanEmdeBoas *>(this->universeRoot, nullptr);
+
+            // cada cluster recebe uma VEB(universe_root)
+            for (int i = 0; i < this->universeRoot; i++)
+                clusters[i] = new VanEmdeBoas(this->universeRoot);
         }
     }
-}
 
-int successor(Van_Emde_Boas *veb, int key)
-{
-
-    // caso base
-    if (veb->universe_size == 2)
+    int remove(int key)
     {
-        if (key == 0 && veb->maximum == 1)
-            return 1;
-        else
-            return -1;
-    }
-
-    // se o min presente é maior que a chave,
-    // então ele é o seu sucessor
-    else if (veb->minimum != veb->universe_size && key < veb->minimum)
-        return veb->minimum;
-
-    else
-    {
-        int max_incluster = maximum(veb->clusters[veb->high(key)]);
-
-        int offset{0}, succ_cluster{0};
-
-        // se o cluster não estiver vazio, encontra o sucessor da chave
-        // dentro dele
-        if (max_incluster != -1 && veb->low(key) < max_incluster)
+        if (this->universeSize == 2)
         {
-
-            offset = successor(
-                veb->clusters[veb->high(key)],
-                veb->low(key));
-
-            return veb->generate_index(veb->high(key), offset);
-        }
-
-        // se nao, procura no proximo cluster nao vazio
-        else
-        {
-
-            succ_cluster = successor(veb->summary, veb->high(key));
-
-            if (succ_cluster == -1)
-                return -1;
-            // encontra o minimo dentro desse cluster
-            else
+            if (key == this->minimum)
             {
-                offset = minimum(veb->clusters[succ_cluster]);
-                return veb->generate_index(succ_cluster, offset);
-            }
-        }
-    }
-}
 
-int predecessor(Van_Emde_Boas *veb, int key)
-{
-    // caso base: se chave 1, e chave 0 esta presente
-    // retorna 0, se nao, retorna -1
-    if (veb->universe_size == 2)
-    {
-        if (key == 1 && veb->minimum == 0)
-            return 0;
-        else
-            return -1;
-    }
+                if (this->minimum != this->maximum)
+                    this->minimum = this->maximum;
 
-    // se a chave é maior que o maximo presente, entao o
-    // maximo é o seu predecessor
-    else if (veb->maximum != -1 && key > veb->maximum)
-        return veb->maximum;
-    
-    else
-    {
-        // checa no cluster da chave se ha algum elemento
-        int min_incluster = minimum(veb->clusters[veb->high(key)]);
-
-        int offset{0}, pred_cluster{0};
-
-        // se ha alguma chave no cluster e key é maior que o minimo presente
-        // significa que o seu predecessor está lá
-        if (min_incluster != veb->universe_size && veb->low(key) > min_incluster)
-        {
-
-            offset = predecessor(
-                veb->clusters[veb->high(key)],
-                veb->low(key));
-
-            return veb->generate_index(veb->high(key), offset);
-        }
-
-        // se nao, procura pelo predecessor do seu cluster no summary
-        // que vai retornar o cluster predecessor que possua uma chave presente 
-        else
-        {
-
-            pred_cluster = predecessor(veb->summary, veb->high(key));
-
-            // se nao ha um cluster predecessor...
-            if (pred_cluster == -1)
-            {
-                if (veb->minimum != veb->universe_size && key > veb->minimum)
-                    return veb->minimum;
-                
                 else
-                    return -1;
+                    this->minimum = this->universeSize;
             }
+            if (key == this->maximum)
+            {
+                if (this->minimum != this->universeSize)
+                    this->maximum = this->minimum;
+                else
+                    this->maximum = -1;
+            }
+            return 1;
+        }
+        if (key == this->minimum)
+        {
+            int i = this->summary->minimum;
 
-            // se ha, acha o predecessor dentro do cluster predecessor
+            // nao ha outro elemento na veb
+            if (i == this->summary->universeSize)
+            {
+                this->minimum = this->universeSize;
+                this->maximum = -1;
+
+                return 1;
+            }
+            // encontra o novo minimo e o remove recursivamente
             else
             {
-                offset = maximum(veb->clusters[pred_cluster]);
-                return veb->generate_index(pred_cluster, offset);
+                this->minimum = this->generate_index(i, this->clusters[i]->minimum);
+                key = this->minimum;
             }
         }
-    }
-}
 
-int remove(Van_Emde_Boas *veb, int key, int nivel = 1)
-{
+        int h = this->high(key);
+        int l = this->low(key);
 
-    // se só há um elemento, significa que é o key
-    if (veb->maximum == veb->minimum)
-    {
-        veb->minimum = veb->universe_size;
-        veb->maximum = -1;
-    }
-    else if (veb->universe_size == 2)
-    {
-        if (key == 0)
-            veb->minimum = 1;
-        else
-            veb->minimum = 0;
+        int result = 1 + this->clusters[h]->remove(l); // adicionar retorno (?)
 
-        veb->maximum = veb->minimum;
-    }
-    else
-    {
-        // se queremos deletar o minimo, procuramos
-        // o seu sucessor dentro da arvore
-        if (key == veb->minimum)
+        // removeu o ultimo elemento do cluster
+        if (this->clusters[h]->minimum == this->clusters[h]->universeSize)
+            this->summary->remove(h);
+
+        // se a chave for o max da estrutura, procura o novo maximo
+        if (key == this->maximum)
         {
+            int i = this->summary->maximum;
+            if (i == -1)
+                this->maximum = this->minimum;
 
-            int first_cluster = minimum(veb->summary);
-
-            key = veb->generate_index(first_cluster, minimum(veb->clusters[first_cluster]));
-
-            veb->minimum = key;
+            else
+                this->maximum = this->generate_index(i, this->clusters[i]->maximum);
         }
 
-        remove(veb->clusters[veb->high(key)], veb->low(key), nivel++);
+        return result;
+    }
 
-        // se apos a remoção, o min se tornou universe_size, precisamos deletar
-        // o cluster do summary também
-        if (minimum(veb->clusters[veb->high(key)]) == veb->universe_size)
+    int insert(int key)
+    {
+        if (this->universeSize == 2)
         {
+            this->minimum = min(this->minimum, key);
+            this->maximum = max(this->maximum, key);
 
-            remove(veb->summary, veb->high(key));
+            return 1;
+        }
 
-            // se a chave é o max da veb tree
-            if (key == veb->maximum)
+        // se veb vazia, min = max = key
+        if (this->minimum == this->universeSize)
+        {
+            this->minimum = key;
+            this->maximum = key;
+
+            return 1;
+        }
+
+        if (key < this->minimum)
+            swap(this->minimum, key);
+
+        this->maximum = max(this->maximum, key);
+
+        int high = this->high(key);
+        int low = this->low(key);
+
+        // se cluster vazio, insere no cluster e no summary
+        if (this->clusters[high]->minimum == this->clusters[high]->universeSize)
+            this->summary->insert(high);
+
+        return (1 + this->clusters[high]->insert(low));
+    }
+
+    bool contains(int key)
+    {
+        // A key está fora do universo
+        if (this->universeSize < key)
+            return false;
+
+        if (this->minimum == key || this->maximum == key)
+            return true;
+        else
+        {
+            // se o tamanho do universo é 2 e a chave
+            // não é nem o max, nem o min, significa que ela
+            // não está presente na estrutura
+            if (this->universeSize == 2)
+                return false;
+            else
+                return this->clusters[this->high(key)]->contains(this->low(key));
+        }
+    }
+
+    int successor(int key)
+    {
+        // caso base
+        if (this->universeSize == 2)
+        {
+            if (key == 0 && this->maximum == 1)
+                return 1;
+            else
+                return -1;
+        }
+        // se o min presente é maior que a chave,
+        // então ele é o seu sucessor
+        else if (this->minimum != this->universeSize && key < this->minimum)
+            return this->minimum;
+
+        else
+        {
+            int max_incluster = this->clusters[this->high(key)]->maximum;
+
+            int offset{0}, succ_cluster{0};
+
+            // se o cluster não estiver vazio, encontra o sucessor da chave
+            // dentro dele
+            if (max_incluster != -1 && this->low(key) < max_incluster)
             {
-                int max_insummary = maximum(veb->summary);
 
-                // max = -1 indica que só há um elemento agora na arvore: o minimo
-                if (max_insummary == -1)
-                    veb->maximum = veb->minimum;
-                // se nao, procura o novo maximo e atribui
+                offset = this->clusters[this->high(key)]->successor(this->low(key));
+
+                return this->generate_index(this->high(key), offset);
+            }
+
+            // se nao, procura no proximo cluster nao vazio
+            else
+            {
+                succ_cluster = this->summary->successor(this->high(key));
+
+                if (succ_cluster == -1)
+                    return -1;
+                // encontra o minimo dentro desse cluster
                 else
                 {
-                    veb->maximum = veb->generate_index(
-                        max_insummary,
-                        maximum(veb->clusters[max_insummary]));
+                    offset = this->clusters[succ_cluster]->minimum;
+                    return this->generate_index(succ_cluster, offset);
                 }
             }
         }
-        else if (key == veb->maximum)
-        {
-            veb->maximum = veb->generate_index(
-                veb->high(key),
-                maximum(veb->clusters[veb->high(key)]));
-        }
     }
-
-    return nivel;
-}
+};
 
 int main()
 {
-    int m, burn_in, n, i, f, d, p;
+    std::cin.tie(nullptr);
+    ui seed, m, burn_in, n, i, f, d, p;
 
-    std::cin >> Seed >> m >> burn_in >> n >> i >> f >> d >> p;
+    std::cin >> seed >> m >> burn_in >> n >> i >> f >> d >> p;
 
     ui u = 1 << (1 << m);
-    Van_Emde_Boas *veb = new Van_Emde_Boas(u);
 
-    Current = Seed;
+    RNG *rng = new RNG(seed);
+    VanEmdeBoas *veb = new VanEmdeBoas(u);
 
     // BURN-IN : inserções
     for (int k = 0; k < burn_in; k++)
-    {
-        if (k == 0)
-            insert(veb, (Seed % u));
-        else
-            insert(veb, (NEXT_RAND() % u));
-    }
+        veb->insert(rng->next() % u);
 
     int should_print = 0;
 
     for (int k = 0; k < n; k++)
     {
-        int x = NEXT_RAND() % (i + f + d);
+        int x = rng->next() % (i + f + d);
+
         should_print = (k % p) == 0 ? 1 : 0;
 
-        // ins
-        if (x < i)
+        if (x < i) // ins
         {
-            ui item = (NEXT_RAND() % u);
+            x = (rng->next() % u);
             int nivel = 0;
 
-            if (!contains(veb, item))
-                nivel = insert(veb, item);
+            if (!veb->contains(x))
+                nivel = veb->insert(x);
 
-            if(should_print)
+            if (should_print)
                 printf("I %d\n", nivel);
         }
-        // suc
-        else if (x < (i + f))
+        else if (x < (i + f)) // suc
         {
-            if(should_print){
-                int suc = successor(veb, NEXT_RAND() % u);
+            x = rng->next() % u;
+
+            if (should_print)
+            {
+                int suc = veb->successor(x);
 
                 if (suc == -1)
                     printf("S %d\n", u);
@@ -365,28 +290,20 @@ int main()
                     printf("S %d\n", suc);
             }
         }
-        // del
-        else
+        else // del
         {
-            int y = NEXT_RAND() % u;
-            x = successor(veb, y);
+            int y = rng->next() % u;
+            int succ = veb->successor(y);
 
-            if (x == -1)
-                x = y;
-            
+            x = succ == -1 ? y : succ;
+
             int nivel = 0;
-            if (contains(veb, x))
-                nivel = remove(veb, x);
-            
-            if(should_print)
+            if (veb->contains(x))
+                nivel = veb->remove(x);
+
+            if (should_print)
                 printf("D %d\n", nivel);
         }
     }
     return 0;
-}
-
-ui NEXT_RAND()
-{
-    Current = (A * Current) + C;
-    return Current;
 }

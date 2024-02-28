54c258b19647d5a228db67c5cee380e5e06745f3
Thayna Santos <tecs@cin.ufpe.br>
2024-02-28 13:42:11 -0300

    all functions added. missing test

--- segTree.c
+++ segTree.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define ui unsigned int
+#define odd(a) ((a % 2) == 1)
 
 struct RNG
 {
@@ -40,16 +41,21 @@ struct SegTree
 {
     int *tree;
     int size;
+    char *func;
 };
 
-void SegTree_init(struct SegTree *st, int size)
+void SegTree_init(struct SegTree *st, int size, char *func)
 {
     st->size = size;
     st->tree = (int *)malloc(2 * size * sizeof(int));
+    st->func = func;
 }
 
-void SegTree_build(struct SegTree *st, int size, char *func)
+void SegTree_build(struct SegTree *st)
 {
+    int size = st->size;
+    char *func = st->func;
+
     for (int i = 0; i < size; i++)
         st->tree[i + size] = (int)(RNG_next(rng) % (4 * size));
 
@@ -66,10 +72,13 @@ void SegTree_build(struct SegTree *st, int size, char *func)
     }
 }
 
-void SegTree_update(struct SegTree *st, int size, char *func)
+void SegTree_update(struct SegTree *st, int i)
 {
+    int size = st->size;
+    char *func = st->func;
+
     int newValue = (int)(RNG_next(rng) % (4 * size));
-    int i = (int)(RNG_next(rng) % size);
+
     i = i + size;
 
     st->tree[i] = newValue;
@@ -91,14 +100,70 @@ void SegTree_update(struct SegTree *st, int size, char *func)
     }
 }
 
-void SegTree_rangeQuery(struct SegTree *st){
+int SegTree_rangeQuery(struct SegTree *st, int l, int r)
+{
+    int size = st->size;
+    char *func = st->func;
 
+    l = size + l;
+    r = size + r;
+
+    int eNeutro;
+    int ans;
+
+    if (strcmp(func, "MIN"))
+        eNeutro = size * 5; // o universo dos valores da arvore é (size * 4), logo size * 5 é muito maior
+
+    else if (strcmp(func, "MAX"))
+        eNeutro = -1;
+
+    else
+        eNeutro = 0;
+
+    ans = eNeutro;
+
+    while (l < r)
+    {
+        if (odd(l)) // l é filho à direita
+        {
+            if (strcmp(func, "MIN"))
+                ans = __min(ans, st->tree[l]);
+
+            else if (strcmp(func, "MAX"))
+                ans = __max(ans, st->tree[l]);
+
+            else
+                ans = ans + st->tree[l];
+
+            l = l + 1;
+        }
+        if (!odd(r)) // r é filho à esquerda
+        {
+            r = r - 1; // sobe um nível
+
+            if (strcmp(func, "MIN"))
+                ans = __min(ans, st->tree[r]);
+
+            else if (strcmp(func, "MAX"))
+                ans = __max(ans, st->tree[r]);
+
+            else
+                ans = ans + st->tree[r];
+        }
+
+        l = l / 2;
+        r = r / 2;
+    }
+
+    return ans;
 }
 
 int main()
 {
     unsigned int s, n, w, q, u, p;
     char f[3];
+
+    int caseCount = 0;
 
     struct SegTree *st;
 
@@ -108,8 +173,11 @@ int main()
         RNG_init(rng, s);
 
         st = (struct SegTree *)malloc(sizeof(struct SegTree));
-        SegTree_init(st, n);
-        SegTree_build(st, n, f);
+        SegTree_init(st, n, f);
+        SegTree_build(st);
+
+        printf("casp %d\n", caseCount);
+        int result;
 
         for (int i = 0; i < w; i++)
         {
@@ -121,17 +189,28 @@ int main()
             else
                 op = 'U'; // Update
 
-            if(op == 'U'){
-                SegTree_update(st, n, f);
+            if (op == 'U')
+            {
+                int index = (int)(RNG_next(rng) % n);
+                SegTree_update(st, index);
+                result = result = SegTree_rangeQuery(st, index, n);
             }
-            else{
-                SegTree_rangeQuery(st);
+            else
+            {
+                int l = (int)(RNG_next(rng) % n);
+                int r = l + 1 + (int)(RNG_next(rng) % (n - l));
+                result = SegTree_rangeQuery(st, l, r);
             }
 
-            // TODO: integralizar o tamanho e a funcao à st
+            if ((i % p) == 0)
+                printf("%d\n", result);
         }
+
+        printf("\n");
 
         free(st);
         free(rng);
+
+        caseCount++;
     }
 }
